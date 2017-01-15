6e59cf09105f13b80068ae363eb12065bec2ec78
Guster Woei <gusterwoei@gmail.com>
2017-01-16 00:30:51 +0800

    added login page

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,20 @@
-// grab our gulp packages
-var gulp  = require('gulp'),
-   gutil = require('gulp-util'),
-   pug = require('gulp-pug');
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var watch = require('gulp-watch');
 
-// create a default task and just log a message
-gulp.task('default', function() {
-   return gutil.log('Gulp is running!')
-});
-
-// pug task (run: gulp pug in CLI)
 gulp.task('pug', function(done) {
-    return gulp.src('src/**/*.pug')
-         .pipe(pug()) // pipe to pug plugin
-         .pipe(gulp.dest('src/pages/'))
-         .on('end', done); // tell gulp our output folder
+    gulp
+        .src('src/pages/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('src/pages/'))
+        .on('end', done);
 });
 
-// watchers
-gulp.task('watch', function () {
-   gulp.watch('gulp-pug', ['pug']);
+gulp.task('watch', function() {
+	watch([ 'src/pages/**/*.pug' ], function() { gulp.start('pug'); });
+});
+
+gulp.task('default', [ 'pug' ], function () {
+	gulp.start('watch');
+	gulp.start('pug');
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DetailsPage } from '../pages/details/details';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     DetailsPage,
+    LoginPage,
   ],
   imports: [
     IonicModule.forRoot(MyApp, {
@@ -20,6 +22,7 @@ import { DetailsPage } from '../pages/details/details';
     MyApp,
     HomePage,
     DetailsPage,
+    LoginPage,
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
