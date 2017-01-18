8ddbb710f16bec9d6ac5023b8354ff48affbf64d
Gusterwoei <kellylu@Gusterwoeis-MacBook-Pro.local>
2017-01-18 18:44:40 +0800

    playing around with config

--- build-android.sh
+++ build-android.sh
@@ -1,4 +1,5 @@
 
-ionic build android --release -- --keystore="platforms/android/keystore.store" --alias=github-finder --storePassword=abc123
+#ionic build android --release -- --keystore="keys/keystore.store" --alias=github-finder --storePassword=abc123
+ionic build android --release --buildConfig=build.json
 
 cp ./platforms/android/build/outputs/apk/android-release.apk ./outputs/android-release.apk
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 var watch = require('gulp-watch');
+var rename = require('gulp-rename');
 
 gulp.task('pug', function(done) {
     gulp
@@ -12,6 +13,18 @@ gulp.task('pug', function(done) {
 
 gulp.task('watch', function() {
 	watch([ 'src/pages/**/*.pug' ], function() { gulp.start('pug'); });
+});
+
+gulp.task('build-dev', function() {
+    gulp.src('app-settings.dev.json')
+    .pipe(rename('app-settings.json'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('build-prod', function() {
+    gulp.src('app-settings.prod.json')
+    .pipe(rename('app-settings.json'))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', [ 'pug' ], function () {
